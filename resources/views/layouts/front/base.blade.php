1a0e6bb538190f2e2713f8632d391059f594b049
EricTascaWebartys <webartys@outlook.com>
2022-12-05 22:53:57 +0100

    update page

--- resources/views/layouts/front/base.blade.php
+++ resources/views/layouts/front/base.blade.php
@@ -12,7 +12,9 @@
              <meta name="robots" content="noindex" />
              <meta name="googlebot" content="noindex">
          @endisset
+    	{{-- <link href='//fonts.googleapis.com/css?family=DM+Sans:400,400i,500,700' rel='stylesheet'> --}}
     	<link rel="stylesheet" href="{{ asset('assets/front/css/style.min.css') }}" />
+    	{{-- <link rel="stylesheet" href="{{ asset('assets/front/css/custom.css') }}" /> --}}
     	<link rel="shortcut icon" href="{{ asset('img/favicon.webp') }}">
     	<link rel="apple-touch-icon" href="{{ asset('img/apple-touch-icon.webp') }}">
     	<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/apple-touch-icon-72x72.webp') }}">
