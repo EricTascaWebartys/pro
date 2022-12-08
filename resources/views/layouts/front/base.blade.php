dd319f417821710ac31b47337d38d8dbd07c2e9d
EricTascaWebartys <webartys@outlook.com>
2022-12-08 10:24:34 +0100

    css min

--- resources/views/layouts/front/base.blade.php
+++ resources/views/layouts/front/base.blade.php
@@ -13,7 +13,7 @@
              <meta name="googlebot" content="noindex">
          @endisset
     	{{-- <link href='//fonts.googleapis.com/css?family=DM+Sans:400,400i,500,700' rel='stylesheet'> --}}
-    	<link defer rel="stylesheet" href="{{ asset('assets/front/css/style.min.css') }}" />
+    	<link async rel="stylesheet" href="{{ asset('assets/front/css/style.min.css') }}" />
     	{{-- <link rel="stylesheet" href="{{ asset('assets/front/css/custom.css') }}" /> --}}
     	<link rel="shortcut icon" href="{{ asset('img/favicon.webp') }}">
     	<link rel="apple-touch-icon" href="{{ asset('img/apple-touch-icon.webp') }}">
